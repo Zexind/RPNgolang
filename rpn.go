04171cf96a3406fd8e13a4eea9bf765c6bbad488
Zexind <qwertyeeee2003@mail.ru>
2024-03-12 20:50:05 +0300

    commit!

--- rpn.go
+++ rpn.go
@@ -1,8 +1,138 @@
 package main
 
-//123123
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
-//todo:move rpn funcs here
-//todo:write tests (читаем выражение из инпута и сравниваем ответы из оутпута и вывод тру/фалсе)
-//todo:refucktor functions
-//todo:remake with stack
+func evaluateRPN(tokens []string) (float64, error) {
+
+	stack := make([]float64, 0)
+
+	for _, token := range tokens {
+		if num, err := strconv.ParseFloat(token, 32); err == nil {
+			stack = append(stack, num)
+		} else {
+			if len(stack) < 2 {
+				return 0, fmt.Errorf("invalid RPN expression")
+			}
+
+			// Pop the last two operands
+			op2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			op1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			// Perform the operation based on the operator
+			switch token {
+			case "+":
+				stack = append(stack, op1+op2)
+			case "-":
+				stack = append(stack, op1-op2)
+			case "*":
+				stack = append(stack, op1*op2)
+			case "/":
+				if op2 == 0 {
+					return 0, fmt.Errorf("division by zero")
+				}
+
+				stack = append(stack, op1/op2)
+			case "^":
+				stack = append(stack, math.Pow(op1, op2))
+			default:
+				return 0, fmt.Errorf("unknown operator: %s", token)
+			}
+		}
+	}
+
+	if len(stack) != 1 {
+		return 0, fmt.Errorf("invalid RPN expression")
+	}
+
+	return stack[0], nil
+}
+
+func getPriority(token string) int {
+
+	m := map[string]int{
+		"+": 1,
+		"-": 1,
+		"*": 2,
+		"/": 2,
+		"^": 3,
+		")": 4,
+		"(": 5,
+	}
+
+	n := m[token]
+	return n
+}
+
+func convertToRPN(tokens []string) []string {
+	signs := NewStack[string]()
+	var result []string
+	var i = 0
+	var priority int
+	var prevPriority = 0
+	prevPrevPriority := 0
+	for i < len(tokens) {
+		priority = getPriority(tokens[i])
+		switch priority {
+		case 0:
+			result = append(result, tokens[i])
+		case 1:
+			if prevPriority == 2 {
+				if signs.Size() == 1 || signs.Size() > 2 {
+					result = append(result, signs.Pop())
+				} else if signs.Size() == 2 {
+					result = append(result, signs.Pop())
+					result = append(result, signs.Pop())
+				}
+			}
+			prevPrevPriority = getPriority(signs.Peek())
+			fmt.Println(prevPrevPriority)
+			if prevPrevPriority == 1 {
+				result = append(result, signs.Pop())
+			}
+			if prevPriority == 3 {
+				result = append(result, signs.Pop())
+			}
+			signs.Push(tokens[i])
+		case 2:
+			prevPrevPriority = getPriority(signs.Peek())
+			fmt.Println(prevPrevPriority)
+			if prevPrevPriority == 1 {
+				signs.Push(tokens[i])
+				break
+			} else if prevPriority >= 2 {
+				result = append(result, signs.Pop())
+			}
+			signs.Push(tokens[i])
+		case 3:
+			signs.Push(tokens[i])
+		case 4:
+			result = append(result, signs.Pop())
+			prevPrevPriority = getPriority(signs.Peek())
+			fmt.Println(prevPrevPriority)
+			if prevPrevPriority >= 4 {
+				signs.Pop()
+			}
+		case 5:
+			signs.Push(tokens[i])
+		}
+		if priority != 0 {
+			prevPriority = priority
+		}
+		i++
+		fmt.Println(signs)
+		fmt.Println(result)
+	}
+
+	for signs.Size() > 0 {
+		result = append(result, signs.Pop())
+	}
+	fmt.Println(result)
+
+	return result
+}
--- teperono.go
+++ teperono.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -30,53 +29,6 @@ func main() {
 		file.WriteString(result1)
 		file.WriteString("\n")
 	}
-}
-
-func evaluateRPN(tokens []string) (float64, error) {
-
-	stack := make([]float64, 0)
-
-	for _, token := range tokens {
-		if num, err := strconv.ParseFloat(token, 32); err == nil {
-			stack = append(stack, num)
-		} else {
-			if len(stack) < 2 {
-				return 0, fmt.Errorf("invalid RPN expression")
-			}
-
-			// Pop the last two operands
-			op2 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			op1 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-
-			// Perform the operation based on the operator
-			switch token {
-			case "+":
-				stack = append(stack, op1+op2)
-			case "-":
-				stack = append(stack, op1-op2)
-			case "*":
-				stack = append(stack, op1*op2)
-			case "/":
-				if op2 == 0 {
-					return 0, fmt.Errorf("division by zero")
-				}
-
-				stack = append(stack, op1/op2)
-			case "^":
-				stack = append(stack, math.Pow(op1, op2))
-			default:
-				return 0, fmt.Errorf("unknown operator: %s", token)
-			}
-		}
-	}
-
-	if len(stack) != 1 {
-		return 0, fmt.Errorf("invalid RPN expression")
-	}
-
-	return stack[0], nil
 }
 
 func getLinesFromFile(adress string) []string {
@@ -131,88 +83,4 @@ func getTokens(a string) []string {
 	fmt.Println(tokens)
 
 	return tokens
-}
-
-func getPriority(token string) int {
-
-	m := map[string]int{
-		"+": 1,
-		"-": 1,
-		"*": 2,
-		"/": 2,
-		"^": 3,
-		")": 4,
-		"(": 5,
-	}
-
-	n := m[token]
-	return n
-}
-
-func convertToRPN(tokens []string) []string {
-	signs := NewStack[string]()
-	var result []string
-	var i = 0
-	var priority int
-	var prevPriority = 0
-	prevPrevPriority := 0
-	for i < len(tokens) {
-		priority = getPriority(tokens[i])
-		switch priority {
-		case 0:
-			result = append(result, tokens[i])
-		case 1:
-			if prevPriority == 2 {
-				if signs.Size() == 1 || signs.Size() > 2 {
-					result = append(result, signs.Pop())
-				} else if signs.Size() == 2 {
-					result = append(result, signs.Pop())
-					result = append(result, signs.Pop())
-				}
-			}
-			prevPrevPriority = getPriority(signs.Peek())
-			fmt.Println(prevPrevPriority)
-			if prevPrevPriority == 1 {
-				result = append(result, signs.Pop())
-			}
-			if prevPriority == 3 {
-				result = append(result, signs.Pop())
-			}
-			signs.Push(tokens[i])
-		case 2:
-			prevPrevPriority = getPriority(signs.Peek())
-			fmt.Println(prevPrevPriority)
-			if prevPrevPriority == 1 {
-				signs.Push(tokens[i])
-				break
-			} else if prevPriority >= 2 {
-				result = append(result, signs.Pop())
-			}
-			signs.Push(tokens[i])
-		case 3:
-			signs.Push(tokens[i])
-		case 4:
-			result = append(result, signs.Pop())
-			prevPrevPriority = getPriority(signs.Peek())
-			fmt.Println(prevPrevPriority)
-			if prevPrevPriority >= 4 {
-				signs.Pop()
-			}
-		case 5:
-			signs.Push(tokens[i])
-		}
-		if priority != 0 {
-			prevPriority = priority
-		}
-		i++
-		fmt.Println(signs)
-		fmt.Println(result)
-	}
-
-	for signs.Size() > 0 {
-		result = append(result, signs.Pop())
-	}
-	fmt.Println(result)
-
-	return result
 }
